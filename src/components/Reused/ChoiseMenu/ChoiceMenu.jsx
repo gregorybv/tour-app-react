8109570a968b838263d26f8e9a178b5c708004cb
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-03-31 08:50:18 +0300

    new edits

--- src/components/Reused/ChoiseMenu/ChoiceMenu.jsx
+++ src/components/Reused/ChoiseMenu/ChoiceMenu.jsx
@@ -1,47 +1,43 @@
-import React from 'react';
-import './choicemenu.css'
+import React from "react"
+import "./choicemenu.css"
 
-const allNavButtons = document.querySelectorAll(".choice__link");
+const allNavButtons = document.querySelectorAll(".choice__link")
 allNavButtons.forEach((button) => {
   button.addEventListener("click", (event) => {
     allNavButtons.forEach((button) => {
-      button.classList.remove("choice__link_active");
-    });
-    const {target} = event;
-    target.classList.add("choice__link_active");
-  });
-});
+      button.classList.remove("choice__link_active")
+    })
+    const { target } = event
+    target.classList.add("choice__link_active")
+  })
+})
 
 const ChoiceMenu = () => {
   return (
-    <div className="choice__menu"
-    >
-      <ul className="choice__list">
+    <div className='choice__menu'>
+      <ul className='choice__list'>
         <li>
-          <button
-            type="button"
-            className="choice__link choice__link_active"
-          >
+          <button type='button' className='choice__link choice__link_active'>
             Популярные
           </button>
         </li>
         <li>
-          <button type="button" className="choice__link">
+          <button type='button' className='choice__link'>
             Авторские
           </button>
         </li>
         <li>
-          <button type="button" className="choice__link">
+          <button type='button' className='choice__link'>
             Походы
           </button>
         </li>
         <li>
-          <button type="button" className="choice__link">
+          <button type='button' className='choice__link'>
             Сплавы
           </button>
         </li>
         <li>
-          <button type="button" className="choice__link">
+          <button type='button' className='choice__link'>
             Велопрогулки
           </button>
         </li>
@@ -51,6 +47,3 @@ const ChoiceMenu = () => {
 }
 
 export default ChoiceMenu
-
-
-
