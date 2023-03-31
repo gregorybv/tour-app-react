757cf5210bc622b99e1bec0f7ff7e9e6d2bcbf10
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-03-31 08:50:11 +0300

    new edits

--- src/components/Reused/ChoiseBtn/ChoiceBtn.jsx
+++ src/components/Reused/ChoiseBtn/ChoiceBtn.jsx
@@ -1,21 +1,14 @@
-import React from 'react';
-import './choicebtn.css'
-import arrow from "../../../assetts/image/block_2/icon/arrow.png";
+import React from "react"
+import "./choicebtn.css"
+import arrow from "../../../assetts/image/block_2/icon/arrow.png"
 
 const ChoiceBtn = () => {
   return (
-    <button type="button" className="choice__button button">
+    <button type='button' className='choice__button button'>
       Подробнее
-      <img
-        className="choice__img"
-        src={arrow}
-        alt="Arrow"
-      />
+      <img className='choice__img' src={arrow} alt='Arrow' />
     </button>
   )
 }
 
 export default ChoiceBtn
-
-
-
