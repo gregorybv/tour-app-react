2417723bda78185a6fbe8e8ac43b5b700560dea2
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-03-31 08:49:58 +0300

    new edits

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,50 +1,26 @@
-import React from 'react';
-import './header.css'
-import fon from '../../assetts/image/block_1/header-img.jpg'
-import Navbar from "../Reused/Navbar/Navbar";
-import HeaderBtn from "../Reused/HeaderBtn/HeaderBtn";
+import React from "react"
+import "./header.css"
+import fon from "../../assetts/image/block_1/header-img.jpg"
+import Navbar from "../Reused/Navbar/Navbar"
+import HeaderBtn from "../Reused/HeaderBtn/HeaderBtn"
 
 const Header = () => {
   return (
-    <header className="header">
-      <div className="header__content">
-        <Navbar/>
-        <div className="header__main">
-          <h1 className="header__title"
-              // data-aos="fade-up"
-              // data-aos-duration="1500"
-          >
-            Идеальные путешествия существуют
-          </h1>
-          <p className="header__subtitle"
-             // data-aos="fade-up"
-             // data-aos-duration="1500"
-          >
-            Идейные соображения высшего порядка, а также рамки и
-            место обучения кадров
+    <header className='header'>
+      <div className='header__content'>
+        <Navbar />
+        <div className='header__main'>
+          <h1 className='header__title'>Идеальные путешествия существуют</h1>
+          <p className='header__subtitle'>
+            Идейные соображения высшего порядка, а также рамки и место обучения
+            кадров
           </p>
-          <HeaderBtn/>
+          <HeaderBtn />
         </div>
       </div>
-      <img
-        className="header__img"
-        src={fon}
-        alt="Fon"
-      />
+      <img className='header__img' src={fon} alt='Fon' />
     </header>
   )
 }
 
 export default Header
-
-
-
-
-
-
-
-
-
-
-
-
