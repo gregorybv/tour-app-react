d96cdd3c40002f42b42e255c49f163e2f5ae412d
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-03-31 08:49:44 +0300

    new edits

--- src/components/Blocks/Travel/Travel.jsx
+++ src/components/Blocks/Travel/Travel.jsx
@@ -1,28 +1,23 @@
-import React from 'react';
-import './travel.css'
-import travel from '../../../assetts/image/block_7/footer photo.jpg'
+import React from "react"
+import "./travel.css"
+import travel from "../../../assetts/image/block_7/footer photo.jpg"
 
 const Travel = () => {
   return (
-    <section className="travel container"
-             data-aos="zoom-in"
-             data-aos-duration="1500"
+    <section
+      className='travel container'
+      data-aos='zoom-in'
+      data-aos-duration='1500'
     >
-      <div className="travel__contents">
-        <img
-          className="travel__img"
-          src={travel}
-          alt="footer photo"
-        />
-        <div className="travel__content"
-        >
-          <h3 className="travel__title">
-            Пора в путешествие вместе с нами!
-          </h3>
-          <p className="travel__subtitle">
+      <div className='travel__contents'>
+        <img className='travel__img' src={travel} alt='footer photo' />
+        <div className='travel__content'>
+          <h3 className='travel__title'>Пора в путешествие вместе с нами!</h3>
+          <p className='travel__subtitle'>
             Напиши на почту и узнай подробности на
-            <a href="mailto:yourtour@gmail.com" className="travel__link"
-            > yourtour@gmail.com
+            <a href='mailto:yourtour@gmail.com' className='travel__link'>
+              {" "}
+              yourtour@gmail.com
             </a>
           </p>
         </div>
@@ -32,6 +27,3 @@ const Travel = () => {
 }
 
 export default Travel
-
-
-
