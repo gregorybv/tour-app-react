67c752282e8308d759477c396c176990804134d5
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-03-31 08:50:02 +0300

    new edits

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import Choice from "../Blocks/Choise/Choice";
-import Tour from "../Blocks/Tour/Tour";
-import Review from "../Blocks/Review/Review";
-import Gallery from "../Blocks/Gallery/Gallery";
-import Story from "../Blocks/Story/Story";
-import Travel from "../Blocks/Travel/Travel";
+import React from "react"
+import Choice from "../Blocks/Choise/Choice"
+import Tour from "../Blocks/Tour/Tour"
+import Review from "../Blocks/Review/Review"
+import Gallery from "../Blocks/Gallery/Gallery"
+import Story from "../Blocks/Story/Story"
+import Travel from "../Blocks/Travel/Travel"
 
 const Main = () => {
   return (
     <main>
-      <Choice/>
-      <Tour/>
-      <Review/>
-      <Gallery/>
-      <Story/>
-      <Travel/>
+      <Choice />
+      <Tour />
+      <Review />
+      <Gallery />
+      <Story />
+      <Travel />
     </main>
   )
 }
 
 export default Main
-
-
-
