2cc9c9bd86c068918a4386964bd28e6d5c737286
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-03-31 08:48:47 +0300

    new edits

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import {useEffect} from "react";
-import Aos from 'aos'
+import { useEffect } from "react"
+import Aos from "aos"
 
-import './App.css';
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import Main from "./components/Main/Main";
+import "./App.css"
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
+import Main from "./components/Main/Main"
 
 function App() {
-
   useEffect(() => {
     Aos.init()
-  }, []);
+  }, [])
 
   return (
     <>
-      <Header/>
-      <Main/>
-      <Footer/>
+      <Header />
+      <Main />
+      <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
